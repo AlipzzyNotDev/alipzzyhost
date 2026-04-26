204e859798c68bbb312102b9689f07bfce099113
AlipzzyNotDev <lalnihbos234@gmail.com>
2026-04-26 19:10:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,13 @@
-<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-database-compat.js"></script>
+const firebaseConfig = {
+  apiKey: "AIzaSyDI5C_DGu-fZOUbr0rw6SkTJw7mFxMYW-w",
+  authDomain: "alipzzyhost.firebaseapp.com",
+  projectId: "alipzzyhost",
+  storageBucket: "alipzzyhost.firebasestorage.app",
+  messagingSenderId: "1087809918545",
+  appId: "1:1087809918545:web:5e27d702d35667caf28a70",
+  measurementId: "G-EJ9239LECR",
+  databaseURL: "https://alipzzyhost-default-rtdb.asia-southeast1.firebasedatabase.app"
+};
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyDI5C_DGu-fZOUbr0rw6SkTJw7mFxMYW-w",
-    authDomain: "alipzzyhost.firebaseapp.com",
-    projectId: "alipzzyhost",
-    storageBucket: "alipzzyhost.firebasestorage.app",
-    messagingSenderId: "1087809918545",
-    appId: "1:1087809918545:web:5e27d702d35667caf28a70",
-    measurementId: "G-EJ9239LECR",
-    databaseURL: "https://alipzzyhost-default-rtdb.asia-southeast1.firebasedatabase.app" 
-  };
-
-  // Inisialisasi Firebase
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.database();
-</script>
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
